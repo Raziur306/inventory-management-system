6f20ee91bbc13e5ca5d3155a129668eecb788165
Md Raziur Rahaman Ronju <raziur.rahaman.ronju@gmail.com>
2022-07-14 01:46:41 +0600

    *

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -14,6 +14,7 @@ class _LoginPageState extends State<LoginPage> {
   final _fromKey = GlobalKey<FormState>();
 
   moveToHome(BuildContext context) async {
+
     if (_fromKey.currentState!.validate()) {
       setState(() {
         _changeButton = true;
